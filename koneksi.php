1016f33fd4c66260b677c652892f65eb22901735
Ripki1310 <ripkipernando074@gmail.com>
2025-11-07 02:23:22 -0800

    Menambahkan file koneksi database

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "root";      
+$pass = "";          
+$db   = "ksi2025";   
+$conn = mysqli_connect($host, $user, $pass, $db);
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+} else {
+    echo "Koneksi ke database berhasil!";
+}
+?>
